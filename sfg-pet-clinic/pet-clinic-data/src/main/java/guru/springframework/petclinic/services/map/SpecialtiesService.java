c60aec09e4451876236b062bae10e6cb9dc135b9
Leo Aguillon <leo.aguillondesigns@gmail.com>
2023-03-30 14:54:40 -0500

    Added Specialties implementation, closes #30

---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/petclinic/services/map/SpecialtiesService.java
@@ -0,0 +1,7 @@
+package guru.springframework.petclinic.services.map;
+
+import guru.springframework.petclinic.model.Specialty;
+import guru.springframework.petclinic.services.CrudService;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/petclinic/services/map/SpecialtyServiceMap.java
@@ -0,0 +1,34 @@
+package guru.springframework.petclinic.services.map;
+
+import guru.springframework.petclinic.model.Specialty;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyServiceMap extends AbstractMapService<Specialty, Long> implements SpecialtiesService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+}
