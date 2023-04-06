0666fed42c9986a9740ea4633e91e63ce56103de
Leo Aguillon <leo.aguillondesigns@gmail.com>
2023-04-06 15:48:02 -0500

    add spring data jpa service for specialties , closes #41, closes #43

---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/petclinic/services/springdatajpa/SpecialtyServiceJpa.java
@@ -0,0 +1,45 @@
+package guru.springframework.petclinic.services.springdatajpa;
+
+import guru.springframework.petclinic.model.Specialty;
+import guru.springframework.petclinic.repositories.SpecialityRepository;
+import guru.springframework.petclinic.services.map.SpecialtyService;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class SpecialtyServiceJpa implements SpecialtyService {
+    private final SpecialityRepository specialityRepository;
+
+    public SpecialtyServiceJpa(SpecialityRepository specialityRepository) {
+        this.specialityRepository = specialityRepository;
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+        Set<Specialty> specialties = new HashSet<>();
+        specialityRepository.findAll().forEach(specialties::add);
+        return specialties;
+    }
+
+    @Override
+    public Specialty findById(Long aLong) {
+        return specialityRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return specialityRepository.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        specialityRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialityRepository.deleteById(aLong);
+    }
+}
