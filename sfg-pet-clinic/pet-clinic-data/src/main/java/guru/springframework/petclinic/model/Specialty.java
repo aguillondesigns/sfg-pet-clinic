b11bcf7372108829d1c0fa0aa5d77760b42a69d9
Leo Aguillon <leo.aguillondesigns@gmail.com>
2023-04-06 14:43:42 -0500

    updated vets with jpa entity mappings, closes #36

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Specialty.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Specialty.java
@@ -1,6 +1,14 @@
 package guru.springframework.petclinic.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.CollectionId;
+
+@Entity
+@Table(name = "specialties")
 public class Specialty extends BaseEntity {
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
