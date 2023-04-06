a7a0aab20a913c3259911a8cbdc59c4a7f424c74
Leo Aguillon <leo.aguillondesigns@gmail.com>
2023-04-06 16:21:45 -0500

    added default profile to map service methods

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/petclinic/services/map/PetTypeServiceMap.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/petclinic/services/map/PetTypeServiceMap.java
@@ -1,11 +1,13 @@
 package guru.springframework.petclinic.services.map;
 
 import guru.springframework.petclinic.model.PetType;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
     @Override
     public Set<PetType> findAll() {
--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/petclinic/services/map/SpecialtyServiceMap.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/petclinic/services/map/SpecialtyServiceMap.java
@@ -1,11 +1,13 @@
 package guru.springframework.petclinic.services.map;
 
 import guru.springframework.petclinic.model.Specialty;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class SpecialtyServiceMap extends AbstractMapService<Specialty, Long> implements SpecialtyService {
     @Override
     public Set<Specialty> findAll() {
